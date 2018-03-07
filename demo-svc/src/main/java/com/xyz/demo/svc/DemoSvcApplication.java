e355796576d608b8f838eba8354430112a750ff2
xyz <xuyongzheng@yiche.com>
2018-03-07 18:01:36 +0800

    feat: basic project

---
+++ demo-svc/src/main/java/com/xyz/demo/svc/DemoSvcApplication.java
@@ -0,0 +1,12 @@
+package com.xyz.demo.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSvcApplication.class, args);
+	}
+}
